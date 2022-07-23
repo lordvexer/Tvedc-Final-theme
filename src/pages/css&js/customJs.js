9fafbfc79f1000780d9552d1ccee7631aa510e15
HamidReza Mahmoudzadeh <h.nypd@yahoo.com>
2022-07-23 12:37:46 +0430

    seprated logtin form added

---
+++ src/pages/css&js/customJs.js
@@ -0,0 +1,8 @@
+function Login () {
+document.getElementById("NormalLogin").style.display='none';
+}
+function ResetPassword () {
+    document.getElementById("AfterLogin").style.display="none"
+    document.getElementById("NormalLogin").style.display="none"
+    document.getElementById("ResetPassword").style.display=""
+}
